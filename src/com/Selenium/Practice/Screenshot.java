064596b95ae987ab4203bb413343ecacf7ea6e68
paranjan1981 <ranjan.pratik@gmail.com>
2018-11-16 20:49:56 +0530

    Update Screenshot.java

--- src/com/Selenium/Practice/Screenshot.java
+++ src/com/Selenium/Practice/Screenshot.java
@@ -1,10 +1,8 @@
 package com.Selenium.Practice;
 import com.Selenium.Practice.FirefoxLaunch;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
